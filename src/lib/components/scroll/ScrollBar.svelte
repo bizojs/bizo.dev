a0f5e76fb086ab2dae3b7b42723e8cd542e29d70
Brad <44570552+bizojs@users.noreply.github.com>
2024-01-29 19:15:21 +0000

    fix: add scrollbar for blog page

---
+++ src/lib/components/scroll/ScrollBar.svelte
@@ -0,0 +1,21 @@
+<script>
+    $: width = 0
+
+    export let color = "bg-export"
+    export let height = "h-1"
+
+    function handleScroll() {
+        const { scrollHeight, clientHeight, scrollTop } = document.documentElement
+        const winScroll = document.body.scrollTop || scrollTop
+        const winHeight = scrollHeight - clientHeight
+        const scrolled = (winScroll / winHeight) * 100
+        width = winHeight > 0 ? scrolled : 0
+    }
+</script>
+
+<div
+    style="width: {width}%"
+    class="{color} {height} m-0 p-0 fixed top-0 left-0 z-[999] transition-[width] duration-200 ease-out"
+/>
+
+<svelte:window on:scroll={handleScroll} />
