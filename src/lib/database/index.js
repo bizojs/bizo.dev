c2524374ec9ed6c094011c825510fb7703a2f782
Brad <44570552+bizojs@users.noreply.github.com>
2024-02-01 11:12:28 +0000

    fix: hacky fix for prisma ESM issue

--- src/lib/database/index.js
+++ src/lib/database/index.js
@@ -1,4 +1,7 @@
-import { PrismaClient } from "@prisma/client"
+// import { PrismaClient } from "@prisma/client"
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
+const { PrismaClient } = require("@prisma/client")
 
 export const db = new PrismaClient()
 export default db
